e82382f7faab123014bae66ab51f453a132e556b
Kushagra Sharma <160770630+Kushagra0909@users.noreply.github.com>
2025-02-21 13:17:53 +0530

    Add files via upload

---
+++ Week3.sql
@@ -0,0 +1,27 @@
+create database Durga_db;
+use Durga_db;
+create table t1(
+name text,
+password varchar(10),
+salary double
+);
+select * from t1;
+use Durga_db;
+create table t2(
+emp_id varchar(5),
+name text,
+password varchar(10),
+salary double
+);
+select * from t2;
+use Durga_db;
+create table Reg(
+emp_id varchar(5),
+name text,
+password varchar(10),
+email text,
+mobile char(10)
+);
+select * from Reg;
+select * from student;
+
